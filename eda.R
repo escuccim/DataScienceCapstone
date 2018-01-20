8d1065dd60daf0306f672103d01ebcbe653e9f5d
Eric Scuccimarra <skooch@gmail.com>
2018-01-20 18:45:53 +0100

    Changes

--- eda.R
+++ eda.R
@@ -2,7 +2,11 @@ library(tm)
 
 # Combine the character vectors into one for analysis
 all <- as.data.frame(c(blog,news,twitter))
+save(all, file="all.RData")
 
 # Create a document term matrix
-dtm <- as.DocumentTermMatrix(all)
+dtm <- as.DocumentTermMatrix(all, weighting=weightTf)
 save(dtm, file="dtm.RData")
+
+# Clean up
+rm("all","blog","news","twitter")
